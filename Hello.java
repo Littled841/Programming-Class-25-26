77c39fb2326f5f74aef22168b2298f72643f874b
Matthew <matthew.dempsey@malad.us>
2025-09-15 10:47:52 -0600

    Created assignment 1.4

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**Matthew Dempsey.... It is for Mr gross Program class
+ * Hello
+*/
+public class Hello {
+
+   public static void main(String[] args) {
+    System.out.println("I have no siblings");
+    System.out.println("My Name is Matthew Dempsey and I am 16");
+
+    System.out.println("My Parents Names are William and Kimberly Dempsey");
+    int momAge=46;
+    int dadAge=45;
+    int momDadCombinedAge=momAge+dadAge;//caculate combined age
+    System.out.println("Mom and dads age combined "+momDadCombinedAge);
+   } 
+}
