9c6bc2f4980e7e73ed0cedaf14326e54decadd54
Matthew <matthew.dempsey@malad.us>
2025-09-22 11:08:15 -0600

    Update assignment21c.java

--- assignment21c.java
+++ assignment21c.java
@@ -3,11 +3,17 @@
 
 public class assignment21c {
     public static void main(String[] args) {
+        int[ ] aray=new int [100];
+        for(int x=1; x<=aray.length; x++){
+            aray [x-1]=x;
+
+
+        }
         int i=0;
         int sum=0;
-        while (i<=100){
-            System.out.print(i +" ");
-            sum=sum+i;
+        while (i<100){
+            System.out.print(aray[i] +" ");
+            sum=sum+aray [i];
             if (i%2==0) {
                 System.out.println("even");
             }else{
