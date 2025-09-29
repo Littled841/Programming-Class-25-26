1faa978f278a72fe4d3769bfcbe7e7de07a813cf
Matthew <matthew.dempsey@malad.us>
2025-09-29 10:47:48 -0600

    Create assignment22c.java

---
+++ assignment22c.java
@@ -0,0 +1,27 @@
+//Matthew Dempsey wrote this
+//This program is for 2.2c assignment
+
+public class assignment22c {
+    public static void main(String[] args) {
+        int[ ] aray=new int [100];
+        for(int x=1; x<=aray.length; x++){
+            aray [x-1]=x;
+
+
+        }
+        int i=0;
+        int sum=0;
+        while (i<100){
+            System.out.print(aray[i] +" ");
+            sum=sum+aray [i];
+            if (i%2==0) {
+                System.out.println("even");
+            }else{
+                System.out.println("odd");
+            }
+
+         i++;
+        }
+        System.out.println("the sum is "+sum);
+    }
+}
