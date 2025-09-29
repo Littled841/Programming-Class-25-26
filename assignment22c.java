2c0c44c61a7ced3da94a05cb6150df4235740c0d
Matthew <matthew.dempsey@malad.us>
2025-09-29 11:26:13 -0600

    Update assignment22c.java

--- assignment22c.java
+++ assignment22c.java
@@ -2,26 +2,35 @@
 //This program is for 2.2c assignment
 
 public class assignment22c {
+    public static int[] swapTwoArrayElements(int[] arrayToSwap, int lowerIndex){
+
+            int temp;
+            temp=arrayToSwap[lowerIndex];
+            arrayToSwap[lowerIndex]=arrayToSwap[lowerIndex+1];
+            arrayToSwap[lowerIndex+1]=temp;
+            return arrayToSwap;
+}
     public static void main(String[] args) {
-        int[ ] aray=new int [100];
-        for(int x=1; x<=aray.length; x++){
-            aray [x-1]=x;
-
+        int[] arrayToSort={1,3,4,5,1,23,57,126,4,543,345,23,12,45,67,97};// this creates the array.
+    System.out.println("unsorted aray");
+     for(int x=0; x<arrayToSort.length; x++){
+        System.out.print(arrayToSort[x] +", ");
+           
+        
 
         }
-        int i=0;
-        int sum=0;
-        while (i<100){
-            System.out.print(aray[i] +" ");
-            sum=sum+aray [i];
-            if (i%2==0) {
-                System.out.println("even");
-            }else{
-                System.out.println("odd");
+    for(int x=0; x<arrayToSort.length-1; x++){
+         for(int y=0; y<arrayToSort.length-1-x; y++){
+             if (arrayToSort[y]>arrayToSort[y+1]) {
+                arrayToSort=swapTwoArrayElements (arrayToSort,y);            
             }
-
-         i++;
         }
-        System.out.println("the sum is "+sum);
     }
+   System.out.println();     
+     System.out.println("sorted aray");
+     for(int x=0; x<arrayToSort.length; x++){
+        System.out.print(arrayToSort[x] +", ");
+
+        }       
+}
 }
